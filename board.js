43865eaec9d04018ccd568b3ba7d8fe27a482ac5
Martin M. S. Pedersen <traxplayer@gmail.com>
2019-08-17 01:56:33 +0200

    refactor

--- board.js
+++ board.js
@@ -46,76 +46,82 @@ class Board {
 		for (var row = this.rows-1; row >=0 && this.board[col + row*this.cols] != "empty" ; row--) { ; } 
 		if (row >= 0) {
 			this.board[col + row*this.cols] = this.whoToMove;
-
-			// check for winning move
-			let winner = (this.whoToMove == "red")?"winner_red":"winner_yellow";
-			if (row <= 2 && 
-					this.whoToMove == this.board[col + (row+1)*this.cols] &&
-					this.whoToMove == this.board[col + (row+2)*this.cols] &&
-					this.whoToMove == this.board[col + (row+3)*this.cols]) 
-			{
-				this.winner = this.whoToMove;
-				this.isGameOver = true;
-				this.board[col + (row+0)*this.cols] = winner;
-				this.board[col + (row+1)*this.cols] = winner;
-				this.board[col + (row+2)*this.cols] = winner;
-				this.board[col + (row+3)*this.cols] = winner;
-				return;
+			checkForWin(col, row);
+			if (this.isGameOver != true) {
+				// switch player
+				this.whoToMove = (this.whoToMove == "red"?"yellow":"red");
 			}
-			let matches = 0;
-			for (let i = 0; i < this.cols; i++) {
-				if (this.whoToMove == this.board[i + row*this.cols]) {
-					matches++;
-					if (matches == 4) {
-						this.winner = this.whoToMove;
-						this.isGameOver = true;
-						this.board[(i-0) + row*this.cols] = winner;
-						this.board[(i-1) + row*this.cols] = winner;
-						this.board[(i-2) + row*this.cols] = winner;
-						this.board[(i-3) + row*this.cols] = winner;
-						return;
-					}
-				} else { matches = 0 }
-			}
-			for (let start_col = 0; start_col + 3 < this.cols; start_col++) {  // NW -> SE
-				for (let start_row = 0; start_row + 3 < this.rows; start_row++) {
-					matches = 0;
-					for (let i = 0; i < 4; i++) {
-						if (this.whoToMove == this.board[(start_col+i) + (start_row+i)*this.cols]) { matches++; }
-						if (matches == 4) {
-							this.winner = this.whoToMove;
-							this.isGameOver = true;
-							this.board[(start_col+0) + (start_row+0)*this.cols] = winner;
-							this.board[(start_col+1) + (start_row+1)*this.cols] = winner;
-							this.board[(start_col+2) + (start_row+2)*this.cols] = winner;
-							this.board[(start_col+3) + (start_row+3)*this.cols] = winner;
-							return;
-						}
-					}
-				}
-			}
-			for (let start_col = 3; start_col + 3 < this.cols; start_col++) {  // NE -> SW
-				for (let start_row = 0; start_row + 3 < this.rows; start_row++) {
-					matches = 0;
-					for (let i = 0; i < 4; i++) {
-						if (this.whoToMove == this.board[(start_col-i) + (start_row+i)*this.cols]) { matches++; }
-						if (matches == 4) {
-							this.winner = this.whoToMove;
-							this.isGameOver = true;
-							this.board[(start_col-0) + (start_row+0)*this.cols] = winner;
-							this.board[(start_col-1) + (start_row+1)*this.cols] = winner;
-							this.board[(start_col-2) + (start_row+2)*this.cols] = winner;
-							this.board[(start_col-3) + (start_row+3)*this.cols] = winner;
-							return;
-						}
-					}
-				}
-			}
-
-			// switch player
-			this.whoToMove = (this.whoToMove == "red"?"yellow":"red");
 		}
 	}
 
 	get(row, col) { return this.board[col + row*this.cols]; }
+	
+	checkForWin(col, row) {
+		if (this.isGameOver == true) return;
+		// check for winning move
+		let winner = (this.whoToMove == "red")?"winner_red":"winner_yellow";
+		if (row <= 2 && 
+				this.whoToMove == this.board[col + (row+1)*this.cols] &&
+				this.whoToMove == this.board[col + (row+2)*this.cols] &&
+				this.whoToMove == this.board[col + (row+3)*this.cols]) 
+		{
+			this.winner = this.whoToMove;
+			this.isGameOver = true;
+			this.board[col + (row+0)*this.cols] = winner;
+			this.board[col + (row+1)*this.cols] = winner;
+			this.board[col + (row+2)*this.cols] = winner;
+			this.board[col + (row+3)*this.cols] = winner;
+			return;
+		}
+		let matches = 0;
+		for (let i = 0; i < this.cols; i++) {
+			if (this.whoToMove == this.board[i + row*this.cols]) {
+				matches++;
+				if (matches == 4) {
+					this.winner = this.whoToMove;
+					this.isGameOver = true;
+					this.board[(i-0) + row*this.cols] = winner;
+					this.board[(i-1) + row*this.cols] = winner;
+					this.board[(i-2) + row*this.cols] = winner;
+					this.board[(i-3) + row*this.cols] = winner;
+					return;
+				}
+			} else { matches = 0 }
+		}
+		for (let start_col = 0; start_col + 3 < this.cols; start_col++) {  // NW -> SE
+			for (let start_row = 0; start_row + 3 < this.rows; start_row++) {
+				matches = 0;
+				for (let i = 0; i < 4; i++) {
+					if (this.whoToMove == this.board[(start_col+i) + (start_row+i)*this.cols]) { matches++; }
+					if (matches == 4) {
+						this.winner = this.whoToMove;
+						this.isGameOver = true;
+						this.board[(start_col+0) + (start_row+0)*this.cols] = winner;
+						this.board[(start_col+1) + (start_row+1)*this.cols] = winner;
+						this.board[(start_col+2) + (start_row+2)*this.cols] = winner;
+						this.board[(start_col+3) + (start_row+3)*this.cols] = winner;
+						return;
+					}
+				}
+			}
+		}
+		for (let start_col = 3; start_col + 3 < this.cols; start_col++) {  // NE -> SW
+			for (let start_row = 0; start_row + 3 < this.rows; start_row++) {
+				matches = 0;
+				for (let i = 0; i < 4; i++) {
+					if (this.whoToMove == this.board[(start_col-i) + (start_row+i)*this.cols]) { matches++; }
+					if (matches == 4) {
+						this.winner = this.whoToMove;
+						this.isGameOver = true;
+						this.board[(start_col-0) + (start_row+0)*this.cols] = winner;
+						this.board[(start_col-1) + (start_row+1)*this.cols] = winner;
+						this.board[(start_col-2) + (start_row+2)*this.cols] = winner;
+						this.board[(start_col-3) + (start_row+3)*this.cols] = winner;
+						return;
+					}
+				}
+			}
+		}
+	} // checkForWin
+
 }
